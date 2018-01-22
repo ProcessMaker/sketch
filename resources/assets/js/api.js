23a5b09857d8fcfb22014d3b925ec6be2b11ae98
Alan Bollinger <velkymx@gmail.com>
2018-01-22 11:16:06 -0800

    Class building

--- resources/assets/js/api.js
+++ resources/assets/js/api.js
@@ -1,9 +1,46 @@
 class Api {
-    /**
-     * Create a new Api instance.
-     */
-    constructor() {
-        
-    }
+  /**
+   * Create a new Api instance.
+   */
+  constructor() {
+
+    this.apiUrl = '',
+    this.apiKey = '',
 
   }
+
+  setUp(apiUrl,apiKey){
+
+    this.apiUrl = apiUrl;
+    this.apiKey = apiKey;
+  }
+
+  data() {
+      return {
+        posts: [],
+        errors: []
+      }
+    },
+
+    // Fetches posts when the component is created.
+    created() {
+      axios.get(this.apiUrl)
+        .then(response => {
+          // JSON responses are automatically parsed.
+          this.posts = response.data
+        })
+        .catch(e => {
+          this.errors.push(e)
+        })
+    },
+
+    methods: {
+      processes: function() {
+
+      },
+      process: function(id) {
+
+      }
+    }
+
+}
